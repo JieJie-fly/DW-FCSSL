ff155e16ef36bee929295c5bb779d0181f856cf2
JieJie-fly <hantf98@tongji.edu.cn>
2024-12-05 11:41:10 +0800

    上传DW-FCSSL

---
+++ utils/data_aug.py
@@ -0,0 +1,163 @@
+from torchvision import transforms
+import torch
+import numpy as np
+import random
+from pathlib import Path
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+import pywt
+
+class wt_mix(object):
+    def __init__(self, rate, dim):
+        self.rate = rate
+        self.dim = dim
+        self.wavelet = 'db2'
+        self.level = 1
+
+    def __call__(self, x):
+        x = torch.tensor(x)
+        x_length = len(x)
+        cof0 = []
+        cof1 = []
+        # cof2 = []
+        recon_signal = []
+
+        for i in range(x.shape[1]):
+            coeffs1 = pywt.wavedec(x[:, i], self.wavelet, level=self.level)
+            cof0.append(coeffs1[0])
+            cof1.append(coeffs1[1])
+            # cof2.append(coeffs1[2])
+
+        for i in range(x.shape[1]):
+            coeffs = []
+            coeffs.append(cof0[i])
+            coeffs.append(random.choice(cof1))
+            # coeffs.append(cof1[i])
+            # coeffs.append(random.choice(cof2))
+            reconstructed_signal = pywt.waverec(coeffs, self.wavelet)
+            recon_signal.append(reconstructed_signal)
+        recon_signal = np.array(recon_signal).T
+        recon_signal = recon_signal[:x_length]
+        return recon_signal
+
+class left_cropping(object):
+    def __init__(self, left_crop):
+        self.left_crop = left_crop
+        self.pil_to_tensor = transforms.ToTensor()
+        self.tensor_to_pil = transforms.ToPILImage()
+
+    def __call__(self, x):
+        x = x[self.left_crop:]
+        return x
+
+class right_cropping(object):
+    def __init__(self, right_crop):
+        self.right_crop = right_crop
+        self.pil_to_tensor = transforms.ToTensor()
+        self.tensor_to_pil = transforms.ToPILImage()
+
+    def __call__(self, x):
+        x = x[:-self.right_crop]
+        return x
+
+class mask(object):
+    def __init__(self, prob):
+        # prob是设置掩码的概率，remain是掩码处的值
+        self.p = prob
+        self.r = 0
+        self.pil_to_tensor = transforms.ToTensor()
+        self.tensor_to_pil = transforms.ToPILImage()
+
+    def __call__(self, X):
+        B, T, C = X.shape
+        # print(B,T,C)
+        mask = np.empty_like(X)
+
+        for b in range(B):
+            ts = X[b, :, 0]
+            ts = np.array(ts)
+            et_num = ts[~np.isnan(ts)].size - self.r
+            total, num = et_num * C, round(et_num * C * self.p)
+
+            while True:
+                i_mask = np.zeros(total)
+                i_mask[random.sample(range(total), num)] = 1
+                i_mask = i_mask.reshape(et_num, C)
+                if 1 not in i_mask.sum(axis=0) and 0 not in i_mask.sum(axis=0):
+                    break
+                break
+
+            i_mask = np.concatenate((i_mask, np.ones((self.r, C))), axis=0)
+            mask[b, ~np.isnan(ts), :] = i_mask
+            mask[b, np.isnan(ts), :] = np.nan
+        # print(mask)
+        # print(X)
+        X = np.array(X)
+        X[mask == 0] = 0
+        return X
+
+
+class Aug():
+    def __init__(self, data_dir, rate, dim, mask_prob, left_crop, right_crop):
+        self.files = list(Path(data_dir).rglob("*.csv"))
+        self.rate = rate
+        self.dim = dim
+        self.prob = mask_prob
+        self.left_crop = left_crop
+        self.right_crop = right_crop
+        self.freq_transform = self.get_simclr_transform()[0]
+        self.crop_transform = self.get_simclr_transform()[1]
+        self.mask_freq_transform = self.get_simclr_transform()[2]
+        self.mask_crop_transform = self.get_simclr_transform()[3]
+        self.raw_transform = self.get_simclr_transform()[4]
+
+    ################################### 只有左剪裁进行频域混合，右剪裁不进行频域混合 ###########################
+    def get_simclr_transform(self):
+        # 先裁剪再频域混合
+        freq_transforms = transforms.Compose([left_cropping(self.left_crop), \
+                                             wt_mix(self.rate, self.dim), transforms.ToTensor()])
+
+        crop_transforms = transforms.Compose([right_cropping(self.right_crop), transforms.ToTensor()])
+
+        mask_freq_transforms = transforms.Compose([freq_transforms, mask(self.prob),\
+                                                  transforms.ToTensor()])
+
+        mask_crop_transforms = transforms.Compose([crop_transforms, mask(self.prob), \
+                                                   transforms.ToTensor()])
+
+        raw_transoforms = transforms.Compose([transforms.ToTensor()])
+
+        return (freq_transforms, crop_transforms, mask_freq_transforms,\
+                mask_crop_transforms, raw_transoforms)
+
+    def __getitem__(self, i):
+        file_i = str(self.files[i])
+        data = pd.read_csv(file_i)
+        data = np.array(data)[:, 1:]   # 尺寸： 30*4
+
+        tensor_freq = self.freq_transform(data)
+        tensor_crop = self.crop_transform(data)
+
+        # 对增强数据进行掩码处理
+        tensor_mask_freq = mask(0.2)(tensor_freq)
+        tensor_mask_crop = mask(0.2)(tensor_crop)
+        tensor_raw = self.raw_transform(data)
+
+        return tensor_freq, tensor_crop, tensor_mask_freq, tensor_mask_crop, tensor_raw
+
+
+
+class CustomDataset(Dataset):
+    def __init__(self, freq, crop, mask_freq, mask_crop):
+        self.freq = freq
+        self.crop = crop
+        self.mask_freq = mask_freq
+        self.mask_crop = mask_crop
+    def __len__(self):
+        return len(self.freq)
+
+    def __getitem__(self, idx):
+        return self.freq[idx], self.crop[idx], self.mask_freq[idx], self.mask_crop[idx]
+
+
---
+++ utils/model.py
@@ -0,0 +1,147 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class SamePadConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, groups=1):
+        super().__init__()
+        self.receptive_field = (kernel_size - 1) * dilation + 1
+        padding = self.receptive_field // 2
+        self.conv = nn.Conv1d(
+            in_channels, out_channels, kernel_size,
+            padding=padding,
+            dilation=dilation,
+            groups=groups
+        )
+        self.remove = 1 if self.receptive_field % 2 == 0 else 0
+
+    def forward(self, x):
+        if x.dim() == 4 and x.size(2) == 1:
+            x = x.squeeze(2)
+        out = self.conv(x)
+        if self.remove > 0:
+            out = out[:, :, : -self.remove]
+        return out
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation, final=False):
+        super().__init__()
+        self.conv1 = SamePadConv(in_channels, out_channels, kernel_size, dilation=dilation)
+        self.conv2 = SamePadConv(out_channels, out_channels, kernel_size, dilation=dilation)
+
+        self.projector = nn.Conv1d(in_channels, out_channels, 1) if in_channels != out_channels or final else None
+
+    def forward(self, x):
+        residual = x if self.projector is None else self.projector(x)
+        x = F.gelu(x)
+        x = self.conv1(x)
+        x = F.gelu(x)
+        x = self.conv2(x)
+        return x + residual
+
+class DilatedConvEncoder(nn.Module):
+    def __init__(self, in_channels, channels, kernel_size):  # (64,24,3)
+        super().__init__()
+        self.net = nn.Sequential(*[
+            ConvBlock(
+                channels[i - 1] if i > 0 else in_channels, channels[i], kernel_size=kernel_size, dilation=2 ** i, final=(i == len(channels) - 1))
+            for i in range(len(channels))
+        ])
+
+    def forward(self, x):
+        return self.net(x)
+
+
+# 数据重构的decoder
+class BertInterpHead(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super().__init__()
+        self.dense = nn.Linear(hidden_dim, 4 * hidden_dim)
+        self.activation = nn.ReLU()
+        self.project = nn.Linear(4 * hidden_dim, input_dim)
+
+    def forward(self, first_token_tensor):
+        # We "pool" the model by simply taking the hidden state corresponding
+        # to the first token.
+        pooled_output = self.dense(first_token_tensor)
+        pooled_output = self.activation(pooled_output)
+        pooled_output = self.project(pooled_output)
+        return pooled_output
+
+class Encoder(nn.Module):
+    def __init__(self, input_dims, output_dims, hidden_dims, depth=5):
+        super().__init__()
+        self.input_dims = input_dims
+        self.output_dims = output_dims
+        self.hidden_dims = hidden_dims
+        self.input_fc = nn.Linear(input_dims, hidden_dims)
+
+        self.feature_extractor = DilatedConvEncoder(
+            # input_dims,
+            hidden_dims,
+            [hidden_dims] * depth + [output_dims],
+            kernel_size=3
+        )
+        self.repr_dropout = nn.Dropout(p=0.1)
+        self.interphead = BertInterpHead(input_dims, output_dims)
+
+
+    def forward(self, x1, x2):  # x: B x T x input_dims     B是batch长度；T是时间长度；
+        x1.to(device)
+        x2.to(device)
+        if self.training:
+            x_whole = self.input_fc(x1.float())
+            x_whole = x_whole.transpose(1, 2)
+            x_whole = self.feature_extractor(x_whole)  # B x Ch x T  20*2*27
+            x_whole = x_whole.transpose(1, 2)  # B x T x Co
+            x_whole = self.repr_dropout(x_whole)
+
+        # recon mask part
+        if self.training:
+            x_interp = self.input_fc(x2.float())
+            x_interp = x_interp.transpose(1, 2)
+            x_interp = self.feature_extractor(x_interp)  # B x Ch x T
+            x_interp = x_interp.transpose(1, 2)  # B x T x Co
+            x_interp = self.repr_dropout(x_interp)
+
+        x = self.input_fc(x1.float())
+        x = x.transpose(1, 2)
+        x = self.feature_extractor(x)  # B x Ch x T
+        x = x.transpose(1, 2)  # B x T x Co
+        x = self.repr_dropout(x)
+
+        if self.training:
+            # interphead(x_interp)是将提取的特征进行重构，Decoder
+            x_recon = self.interphead(x_interp)
+            return x_whole, x_recon
+        else:
+            return x, self.interphead(x)
+
+
+# 4*4*64
+class FCSSL(nn.Module):
+    def __init__(self, input_dims, output_dims, hidden_dims):
+        super(FCSSL, self).__init__()
+        self.extractor = Encoder(input_dims, output_dims, hidden_dims)
+        self.flattern = nn.Flatten(1)
+        self.predictor = nn.Sequential()
+        self.predictor.add_module('1', nn.Linear(output_dims * 10, 64))
+        # self.predictor.add_module('2', nn.Dropout(0.8))
+        self.predictor.add_module('3', nn.Linear(64, 16))
+        self.predictor.add_module('2', nn.ReLU())
+        self.predictor.add_module('4', nn.Linear(16, 1))
+
+    def forward(self, x, x2):
+        # self.extractor.eval()
+        if self.extractor.eval():
+            out1, recon_fea = self.extractor(x, x2)
+            out2 = self.flattern(out1)
+            out3 = self.predictor(out2)
+
+        else:
+            out1, recon_fea = self.extractor(x, x2)
+            out2 = self.flattern(out1)
+            out3 = self.predictor(out2)
+
+        return out3, out1, recon_fea
